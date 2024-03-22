bd334a0b5ed2593c66fb8228683fb65264afd807
Milkman <loganescobarc@gmail.com>
2024-03-22 10:48:11 -0600

    Added exercise 1

---
+++ training-exercises/001/001.py
@@ -0,0 +1,17 @@
+# Haz un script con una funcion que tome 2 numeros de la terminal
+# regrese la division de ambos numeros si la numa de ambos es menor a 100
+# regrese la suma de ambos numeros si la suma es mayor a 100
+
+# Hints:
+
+input()
+print()
+def funcion():
+    condicion1 = 
+    condicion2 = 
+    resultado = 
+    if condicion1 and condicion2:
+        # haz x
+    else:
+        # haz y
+return resultado
