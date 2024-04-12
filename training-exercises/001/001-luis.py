f39560e7f8589385be3ac9287d3b68558ae24a81
Razague <razague@gmail.com>
2024-04-12 11:34:41 -0600

    archivo 001luis  (tarea1) agregado

---
+++ training-exercises/001/001-luis.py
@@ -0,0 +1,31 @@
+#Resolver aqui Luis
+
+# Haz un script con una funcion que tome 2 numeros de la terminal
+# regrese la division de ambos numeros si la numa de ambos es menor a 100
+# regrese la suma de ambos numeros si la suma es mayor a 100
+
+# Hints:
+
+input()
+print()
+def funcion():
+    condicion1 = 
+    condicion2 = 
+    resultado = 
+    if condicion1 and condicion2:
+        # haz x
+    else:
+        # haz y
+return resultado
+
+x = float(1)
+y = float(2)
+
+z = x + y
+w = x / y
+
+if z<100 :
+    w
+else:
+    z 
+return resultado
