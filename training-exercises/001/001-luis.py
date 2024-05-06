d50983cfcee1a514b2951f93d1dff2f47a4abd52
Razague <razague@gmail.com>
2024-05-06 14:18:23 -0600

    Version 2 tarea 1

--- training-exercises/001/001-luis.py
+++ training-exercises/001/001-luis.py
@@ -1,24 +1,17 @@
-#Resolver aqui Luis
 
-# Haz un script con una funcion que tome 2 numeros de la terminal
-# regrese la division de ambos numeros si la numa de ambos es menor a 100
-# regrese la suma de ambos numeros si la suma es mayor a 100
-
-# Hints:
 
 def funcion():
     x = float(input("Escribe un número"))
     y = float(input("Escribe un número"))
 
-    z = x + y
-    w = x / y
-
-    if z<100:
-        return print(w)
+    if x+y<100:
+        w = x / y
+        print(w)
     else:
-        return print(z)
+        w = x + y
+        print(w)
+    return w
 
 funcion()
 
 
-#aaaaa
