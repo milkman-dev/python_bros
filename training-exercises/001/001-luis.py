41dda13eb41c02a8ab93d2201d1c4ae1365de39b
Razague <razague@gmail.com>
2024-06-21 11:09:57 -0600

    arrays clase

--- training-exercises/001/001-luis.py
+++ training-exercises/001/001-luis.py
@@ -15,3 +15,29 @@ def funcion():
 funcion()
 
 
+lista = [0,1,2,3,4,5,6,7,8,9]
+
+print(lista[2])
+
+lista.append(10) # agregar elementos
+print(lista)
+
+
+#dictionary tienen propiedad llave y valor
+diccionario = {1:"uno",2:"dos",3:[1,2,3]}  # las llaves deben de ser del mismo tipo simempre (pero pueden ser de cualquier tipo)
+
+print(diccionario[3])
+
+
+#loops   for loop
+
+for i in lista:
+    print(i)
+
+
+
+#listas dos dimensiones
+
+lista2d = [["A","B","C","D",""],[24,23,22,21,0,0,0,0,0,0,0,0,0,0,0,0],[True,True,True,True,True,True,True]]
+
+print(lista2d[0][1])
